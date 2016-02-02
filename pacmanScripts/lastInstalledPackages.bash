0c85daff52df0e47dcc5a80e01af600e5f4b665a
adrienJarretier <jarretier.adrien@gmail.com>
2016-02-03 00:20:39 +0100

    count and concatenates pair lines

--- pacmanScripts/lastInstalledPackages.bash
+++ pacmanScripts/lastInstalledPackages.bash
@@ -3,10 +3,22 @@ pacman -Qei | grep "Name\|Install Date" > tmp
 old_IFS=$IFS
 IFS=$'\n'
 
+count=1
+
+packageInfos=""
+
 for line in $(cat tmp)
 do
-	echo $line
+	if [ $count -eq 1 ]
+		then
+		packageInfos=$line
+	else
+		packageInfos=$packageInfos" | "$line
+		echo $packageInfos
+		count=0
+	fi
+	((count++))
 done
 IFS=$old_IFS
 
-# echo "" > tmp
+echo "" > tmp
