4bd7015ef5da095c20aab767bf509263cb97d514
adrienJarretier <jarretier.adrien@gmail.com>
2016-02-04 16:48:32 +0100

    test not installed packages

--- pacmanScripts/checkUpdates.bash
+++ pacmanScripts/checkUpdates.bash
@@ -1,12 +1,15 @@
 TMP_DIR=$(mktemp -dp .)
 
-echo "n" | sudo pacman -Syu > $TMP_DIR/tmp
+UPDATE_COMMAND="sudo pacman -Syu chuck"
+
+echo "n" | $UPDATE_COMMAND > $TMP_DIR/tmp
 cat $TMP_DIR/tmp | grep Packages | sed "s/Packages (.*) //" > $TMP_DIR/updates.txt
 
 
 echo "packages wich are not dependencies : "
-for word in $(cat $TMP_DIR/updates.txt)
+for packageNewVersion in $(cat $TMP_DIR/updates.txt)
 do
+	word=$packageNewVersion
 	pacman -Qi $word 2> /dev/null
 	while [ $? -eq 1 ] && [ ${#word} -gt 1 ]
 	do
@@ -15,9 +18,14 @@ do
 	done
 
 	if [ $(echo "$ouput" | grep -c "Required By  *:  *None") -eq 1  ]
-	then
+		then
 		echo "$ouput" | grep --color=always "Name\|Description\|Optional For\|Install Date\|Install Reason"
 		echo "-------------------------------------------------------------------------------"
+	elif [ ${#word} -gt 1 ]
+		then
+		echo "required by another package : "$word
+	else
+		echo "not installed yet : "$packageNewVersion
 	fi
 done
 
