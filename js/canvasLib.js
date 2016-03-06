6b0765e0a8ae8318aaa4f49af96e06fd50def7ec
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-03-06 05:21:48 +0100

    particles can move

--- js/canvasLib.js
+++ js/canvasLib.js
@@ -13,9 +13,11 @@ var Canvas = {
 
 	    this.circle = _cirle; // Circle
 	    this.color = _color; // String
-	    this.velocity = _velocity.times(1/1000); // Point
+	    this.velocity = _velocity.times(1/1000); // Point in pixels/ms
 
 	    this.speed = Math.sqrt(this.velocity.x*this.velocity.x+this.velocity.y*this.velocity.y);
+
+        this.lastTimeMoved = (new Date()).getTime();
 
 	    this.draw = function() {
 
@@ -30,6 +32,41 @@ var Canvas = {
             context.closePath();
 
 	    }
+
+        this.move = function() {
+
+            const newTime = (new Date()).getTime();
+            const deltaT = newTime-this.lastTimeMoved;
+            this.lastTimeMoved = newTime;
+            const deltaVelocity = this.velocity.times( deltaT );
+
+
+            if( ( this.circle.position.x + deltaVelocity.x + this.rayon > theCanvas.width
+                    && this.velocity.x > 0
+                )
+             || (this.circle.position.x + deltaVelocity.x - this.rayon < 0
+                    && this.velocity.x < 0
+                ) ) {
+
+                this.velocity.x *= -1;
+
+            }
+
+            if( ( this.circle.position.y + deltaVelocity.y + this.rayon > theCanvas.height
+                    && this.velocity.y > 0
+                )
+             || (this.circle.position.y + deltaVelocity.y - this.rayon < 0
+                    && this.velocity.y < 0
+                ) ) {
+
+                this.velocity.y *= -1;
+
+            }
+
+            this.circle.position.x += Math.floor(deltaVelocity.x);
+            this.circle.position.y += Math.floor(deltaVelocity.y);
+
+        }
 
 	}
 
